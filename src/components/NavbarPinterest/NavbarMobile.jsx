bc40e1485ae817926cdfee954640cc74fb17a44f
Lucas Cabral <lucassimple@hotmail.com>
2023-12-15 04:29:00 -0300

    Version 1.0.4

--- src/components/NavbarPinterest/NavbarMobile.jsx
+++ src/components/NavbarPinterest/NavbarMobile.jsx
@@ -10,7 +10,8 @@ export default function NavbarMobile() {
 
             <Link to={"/"}>
                 <div className="logo-e-imagen">
-                    <img className="logo-e-imagen-img" src="https://upload.wikimedia.org/wikipedia/commons/9/94/Pinterest_logo.png" alt="Logo de Pinterest" />
+                    {/* <img className="logo-e-imagen-img" src="https://upload.wikimedia.org/wikipedia/commons/9/94/Pinterest_logo.png" alt="Logo de Pinterest" /> */}
+                    <img className="logo-e-imagen-img" src="/img/logoo-pinterest.png" alt="Logo de Pinterest" />
                 </div>
             </Link>
 
@@ -39,9 +40,6 @@ export default function NavbarMobile() {
                     </div>
                 </Link>
             </div>
-
-
-
 
         </div>
     )
--- src/components/Search/Carousel.jsx
+++ src/components/Search/Carousel.jsx
@@ -3,8 +3,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import "./Carousel.scss";
 import Carrusel from "../../JSON/Carousel.json"
+import { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 export default function Carousel() {
+    const [skeleton, setSkeleton] = useState(true)
 
     const settings = {
         dots: true,
@@ -16,17 +19,29 @@ export default function Carousel() {
         autoplaySpeed: 2000,
     };
 
+    useEffect(() => {
+       setTimeout(() => {
+         setSkeleton(false)
+       }, 400);
+    }, [skeleton])
+
     return (
         <div className="carousel" style={{ overflow: "hidden" }}>
 
             <Slider className='carousel-container' {...settings}>
-                {Carrusel.map((item, key) => (
-                    <div key={key} className="d-flex justify-content-center align-items-center container-img">
-                        <img style={{ width: "100%" }} src={item.imagen} alt={item.titulo} />
-                        <strong className="titulo"> {item.titulo} </strong>
-                        <p className='texto'> {item.descripcion} </p>
+                {skeleton ? (
+                    <div className="d-flex justify-content-center align-items-center container-img">
+                        <Skeleton width={800} height={276} />
                     </div>
-                ))}
+                ) : (
+                    Carrusel.map((item, key) => (
+                        <div key={key} className="d-flex justify-content-center align-items-center container-img">
+                            <img style={{ width: "100%" }} src={item.imagen} alt={item.titulo} />
+                            <strong className="titulo"> {item.titulo} </strong>
+                            <p className='texto'> {item.descripcion} </p>
+                        </div>
+                    ))
+                )}
             </Slider>
 
         </div>
